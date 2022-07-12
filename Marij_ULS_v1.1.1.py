2bba4e2c839bd02ca56f6070301dc48f0b1ba5b9
Marij <marij.mtq@gmail.com>
2022-07-12 13:42:39 +0100

    update

---
+++ Marij_ULS_v1.1.1.py
@@ -0,0 +1,20 @@
+#ULTIMATE LEARNING SYSTEM
+#Marij Qureshi
+#Version 1.1.1
+#Total Hours contributed:
+#------------ChangeLog------------
+#1.1.1
+#
+#First Release!
+#---------------------------------
+
+from tkinter import *
+
+root = Tk()
+
+topframe=Frame(root)
+topframe.pack()
+bottomframe=Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+root.mainloop()
