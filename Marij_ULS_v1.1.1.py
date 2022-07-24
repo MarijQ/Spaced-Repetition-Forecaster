4cf48b301cc53ad753e7adc8872ce6bdad6d91d8
Marij <marij.mtq@gmail.com>
2022-07-24 13:54:35 +0100

    update

--- Marij_ULS_v1.1.1.py
+++ Marij_ULS_v1.1.1.py
@@ -1,20 +1,80 @@
-#ULTIMATE LEARNING SYSTEM
-#Marij Qureshi
-#Version 1.1.1
-#Total Hours contributed:
-#------------ChangeLog------------
-#1.1.1
-#
-#First Release!
-#---------------------------------
+from copy import deepcopy
 
-from tkinter import *
 
-root = Tk()
+class SystemState():
+    def __init__(self):
+        self.forecast = [[]]
+        self.maxPerDay = 2
+        self.maxBlendDays = 1
+        self.numCards = 0
 
-topframe=Frame(root)
-topframe.pack()
-bottomframe=Frame(root)
-bottomframe.pack(side=BOTTOM)
+    def addCard(self, date):
+        while len(self.forecast) < date + 1:
+            self.addDay()
+        newCard = Card(self.numCards + 1, date)
+        self.numCards += 1
+        self.forecast[date].append(newCard)
+        futureDeferrals = [card.deferral for day in self.forecast for card in day if
+                           card.studyDay >= date]
+        if max(futureDeferrals) > self.maxBlendDays:
+            self.removeFullCard(newCard.cardID)
+            self.numCards -= 1
+            print("card removed")
 
-root.mainloop()
+    def removeFullCard(self, cardID):
+        self.forecast = [[x for x in day if x.cardID != cardID] for day in self.forecast]
+
+    def addDay(self):
+        self.forecast.append([])
+
+    def promoteCards(self, day):
+        for card in self.forecast[day]:
+            nextDate = day + int(card.nextInterval)
+            deferral = 0
+            while len(self.forecast) >= nextDate + deferral + 1 and len(
+                    self.forecast[nextDate + deferral]) >= self.maxPerDay:
+                deferral += 1
+            if len(self.forecast) >= nextDate + deferral + 1:
+                newCard = deepcopy(card)
+                newCard.nextInterval *= newCard.multiplier
+                newCard.deferral = deferral
+                self.forecast[nextDate + deferral].append(newCard)
+
+    def printArray(self):
+        data = [[(card.cardID, int(card.nextInterval), card.deferral) for card in day] for
+                day
+                in self.forecast]
+        latestCard = 0
+        for day in range(len(data)):
+            cardsAdded = max(x[0] for x in data[day]) - latestCard
+            latestCard = max(latestCard, max(x[0] for x in data[day]))
+            if cardsAdded <= 0:
+                cardsAdded = ""
+            else:
+                cardsAdded = "\t added " + str(cardsAdded)
+            print(day, data[day], cardsAdded)
+            if max(x[2] for x in data[day]) > self.maxBlendDays:
+                print(max(x[2] for x in data[day]))
+
+
+class Card():
+    def __init__(self, cardID, studyday):
+        self.multiplier = 2.5
+        self.studyTime = 1
+        self.cardID = cardID
+        self.nextInterval = 1
+        self.deferral = 0
+        self.studyDay = studyday
+
+
+s = SystemState()
+day = 0
+forecastDays = 30
+for i in range(forecastDays - 1):
+    s.addDay()
+while day < forecastDays:
+    if len(s.forecast[day]) < s.maxPerDay:
+        s.addCard(day)
+    s.promoteCards(day)
+    day += 1
+s.printArray()
