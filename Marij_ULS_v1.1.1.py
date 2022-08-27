5d82421755b6c016a8a29a7b403051b9794f9ccf
Marij <marij.mtq@gmail.com>
2022-08-27 21:08:25 +0100

    update

--- Marij_ULS_v1.1.1.py
+++ Marij_ULS_v1.1.1.py
@@ -7,74 +7,95 @@ class SystemState():
         self.maxPerDay = 2
         self.maxBlendDays = 1
         self.numCards = 0
+        self.lastForecastDate = 0
+        self.cardRejected = 0
 
     def addCard(self, date):
-        while len(self.forecast) < date + 1:
-            self.addDay()
         newCard = Card(self.numCards + 1, date)
+        deferral = 0
+        while len(self.forecast[date + deferral]) >= self.maxPerDay:
+            deferral += 1
+        if date + deferral > self.lastForecastDate:
+            return
+        self.forecast[date + deferral].append(newCard)
         self.numCards += 1
-        self.forecast[date].append(newCard)
-        futureDeferrals = [card.deferral for day in self.forecast for card in day if
-                           card.studyDay >= date]
-        if max(futureDeferrals) > self.maxBlendDays:
-            self.removeFullCard(newCard.cardID)
-            self.numCards -= 1
-            print("card removed")
+        return newCard
 
-    def removeFullCard(self, cardID):
-        self.forecast = [[x for x in day if x.cardID != cardID] for day in self.forecast]
+    def removeFullCard(self, targetID, date=0):
+        self.forecast = [[card for card in day if card.cardID != targetID or
+                          card.studyDate <= date] for day in self.forecast]
+        self.numCards -= sum(1 for day in self.forecast for card in day if
+                             card.cardID == targetID)
 
     def addDay(self):
         self.forecast.append([])
 
-    def promoteCards(self, day):
-        for card in self.forecast[day]:
-            nextDate = day + int(card.nextInterval)
+    def promoteCardFull(self, card):
+        activeCard = card
+        nextDate = card.studyDate + int(activeCard.nextInterval)
+        while nextDate <= self.lastForecastDate:
             deferral = 0
-            while len(self.forecast) >= nextDate + deferral + 1 and len(
-                    self.forecast[nextDate + deferral]) >= self.maxPerDay:
+            while len(self.forecast[nextDate + deferral]) >= self.maxPerDay:
                 deferral += 1
-            if len(self.forecast) >= nextDate + deferral + 1:
-                newCard = deepcopy(card)
+            if deferral <= self.maxBlendDays:
+                if nextDate + deferral + 1 > self.lastForecastDate:
+                    return
+                newCard = deepcopy(activeCard)
                 newCard.nextInterval *= newCard.multiplier
                 newCard.deferral = deferral
+                newCard.studyDate = nextDate + deferral
                 self.forecast[nextDate + deferral].append(newCard)
+                activeCard = newCard
+                nextDate = activeCard.studyDate + int(activeCard.nextInterval)
+            else:
+                self.removeFullCard(card.cardID, date=card.studyDate)
+                print(f"Card {card.cardID} rejected with deferral {deferral}")
+                self.cardRejected = 1
+                return
+
+    def submitCard(self, day):
+        newCard = self.addCard(day)
+        self.promoteCardFull(newCard)
 
     def printArray(self):
-        data = [[(card.cardID, int(card.nextInterval), card.deferral) for card in day] for
-                day
-                in self.forecast]
+        data = [[(card.cardID, int(card.nextInterval), card.deferral) \
+                 for card in day] for day in self.forecast]
         latestCard = 0
         for day in range(len(data)):
-            cardsAdded = max(x[0] for x in data[day]) - latestCard
-            latestCard = max(latestCard, max(x[0] for x in data[day]))
+            if len(data[day]) == 0:
+                cardsAdded = -1
+            else:
+                cardsAdded = max(x[0] for x in data[day]) - latestCard
+                latestCard = max(latestCard, max(x[0] for x in data[day]))
             if cardsAdded <= 0:
                 cardsAdded = ""
             else:
                 cardsAdded = "\t added " + str(cardsAdded)
             print(day, data[day], cardsAdded)
-            if max(x[2] for x in data[day]) > self.maxBlendDays:
-                print(max(x[2] for x in data[day]))
 
 
 class Card():
-    def __init__(self, cardID, studyday):
+    def __init__(self, cardID, studyDate):
         self.multiplier = 2.5
         self.studyTime = 1
         self.cardID = cardID
         self.nextInterval = 1
         self.deferral = 0
-        self.studyDay = studyday
+        self.studyDate = studyDate
 
 
 s = SystemState()
 day = 0
-forecastDays = 30
+forecastDays = 10
 for i in range(forecastDays - 1):
     s.addDay()
-while day < forecastDays:
-    if len(s.forecast[day]) < s.maxPerDay:
-        s.addCard(day)
-    s.promoteCards(day)
-    day += 1
+s.lastForecastDate = len(s.forecast) - 1
+# while day < forecastDays:
+#     while s.cardRejected == 0:
+#         s.submitCard(day)
+#     s.cardRejected = 0
+#     day += 1
+s.submitCard(0)
+s.submitCard(0)
+s.submitCard(0)
 s.printArray()
